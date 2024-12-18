5fde7cb9b92963d65e46554cd646b0444eb9f5a4
Pabbraga <pabbragadev@gmail.com>
2024-12-17 23:04:22 -0300

    first commit

---
+++ src/main/java/com/pabbraga/peritech/PeritechApplication.java
@@ -0,0 +1,13 @@
+package com.pabbraga.peritech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeritechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeritechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pabbraga/peritech/PeritechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pabbraga.peritech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeritechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
